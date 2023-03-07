966e6c1b3c81f345b0d98430c02c426a521ff97f
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-03-07 17:48:15 +0100

    Update install_microk8s.sh

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -9,3 +9,6 @@ sudo microk8s kubectl get all --all-namespaces
 
 echo "alias kubectl='sudo microk8s kubectl'" | sudo tee -a /home/$USER/.bash_aliases
 source /home/$USER/.bash_aliases
+
+KUBECONFIG=`ls /var/snap/microk8s/$(ls /var/snap/microk8s/ | grep [0000-9999])/credentials/client.config`
+sudo cp $KUBECONFIG /home/$USER/kubeconfig
