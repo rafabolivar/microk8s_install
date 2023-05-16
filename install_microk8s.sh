84c6446d0a5979a13c16e4f3bbe3b526ca4b4c0d
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2023-05-16 15:03:45 +0200

    Update install_microk8s.sh
    Install Kubectl

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -7,8 +7,16 @@ sudo snap install microk8s --classic
 sudo microk8s enable dashboard dns registry istio
 sudo microk8s kubectl get all --all-namespaces
 
-echo "alias kubectl='sudo microk8s kubectl'" | sudo tee -a /home/$USER/.bash_aliases
-source /home/$USER/.bash_aliases
+#echo "alias kubectl='sudo microk8s kubectl'" | sudo tee -a /home/$USER/.bash_aliases
+#source /home/$USER/.bash_aliases
 
-KUBECONFIG=`ls /var/snap/microk8s/$(ls /var/snap/microk8s/ | grep [0000-9999])/credentials/client.config`
-sudo cp $KUBECONFIG /home/$USER/kubeconfig
+sudo microk8s config > /home/$USER/kubeconfig
+
+# Install Kubectl
+
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
